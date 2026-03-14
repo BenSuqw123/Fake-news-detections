3162120c80885a67d98ca3c41c86e5686298fd68
kimnganvvv <ntkn26062005@gmail.com>
2026-03-14 12:23:41 +0700

    update merge

--- RAG-LAW&HISTORY/Data/LAW/m.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Đọc file JSONL
-df = pd.read_json("D:\Fake-news-detections\RAG\LAW\courtlistener_6000_clean.jsonl", lines=True)
-
-# Lưu sang Parquet (cần cài đặt: pip install pyarrow)
-df.to_parquet("D:\Fake-news-detections\RAG\LAW\courtlistener_6000_clean.parquet", index=False)
